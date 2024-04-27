5ddaa918212810f02714e8c0b227c97c17e2fc13
Abdi Berhe <abdiberhe@gmail.com>
2024-04-27 10:21:02 +0300

    Update database path in development settings

--- backend/core/settings/development.py
+++ backend/core/settings/development.py
@@ -14,6 +14,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "../", "static")
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR.parent / 'db.sqlite3',
     }
 }
