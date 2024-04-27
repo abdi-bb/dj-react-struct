d84c63fc8f139523f846cb2493b6ad44d1e8fa70
Abdi Berhe <abdiberhe@gmail.com>
2024-04-27 12:46:25 +0300

    Update entrypoint.sh to uncomment production code

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-echo 'Waiting for postgres...'
+# Uncomment for production
+# echo 'Waiting for postgres...'
 
-while ! nc -z $DB_HOSTNAME $DB_PORT; do
-    sleep 0.1
-done
+# while ! nc -z $DB_HOSTNAME $DB_PORT; do
+#     sleep 0.1
+# done
 
-echo 'PostgreSQL started'
+# echo 'PostgreSQL started'
 
 echo 'Running migrations...'
 python manage.py migrate
